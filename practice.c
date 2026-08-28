6f8fa5d840c3e163ffc1cb2388bf461e736b0a6a
sughosh-panigrahi <sughosh.panigrahi@gmail.com>
2026-08-28 09:59:04 +0530

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+int num;
+int remainder;
+int original;
+int reversednum=0;
+printf("the number is :%d",num);
+scanf("%d",&num);
+num=original;
+if(num==reversednum){
+printf("the number is palindromic :%d");
+}
+else{printf(" the number is not palindromic");
+}
+return 0;
+}
